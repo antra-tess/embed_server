bdc1bcf9aa581504ad24b7579f9e536ab698e6c9
ampdot <ampdot@mesh.host>
2023-12-07 15:42:58 -0800

    Add Mac GPU support; reformat

--- embed_server.py
+++ embed_server.py
@@ -19,6 +19,7 @@ from cache import SQLiteCacheBackend, CacheBackend
 
 # todo: autoboot and scale to zero
 
+
 class EncodeRequest(BaseModel):
     input: List[str]
 
@@ -28,27 +29,40 @@ app = FastAPI()
 
 class E5Model:
     @staticmethod
-    def average_pool(last_hidden_states: Tensor,
-                     attention_mask: Tensor) -> Tensor:
-        last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
+    def average_pool(last_hidden_states: Tensor, attention_mask: Tensor) -> Tensor:
+        last_hidden = last_hidden_states.masked_fill(
+            ~attention_mask[..., None].bool(), 0.0
+        )
         return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
 
-    def __init__(self, model, device='cuda'):
+    def __init__(self, model, device="cuda"):
         self.tokenizer = AutoTokenizer.from_pretrained(model)
-        if device == 'cpu':
+        if device == "cpu":
             self.model = AutoModel.from_pretrained(model)
         else:
             self.model = AutoModel.from_pretrained(model)
-        if device is None or device == 'cuda':
-            self.model = self.model.to('cuda')
+            self.model = self.model.to(device)
         self.device = device
 
     def _encode(self, input_texts: List[str]) -> np.array:
-        batch_dict = self.tokenizer(["query: " + s if not (s.startswith('query: ') or s.startswith('passage: ')) else s for s in input_texts], max_length=512, padding=True, truncation=True, return_tensors='pt')
+        batch_dict = self.tokenizer(
+            [
+                "query: " + s
+                if not (s.startswith("query: ") or s.startswith("passage: "))
+                else s
+                for s in input_texts
+            ],
+            max_length=512,
+            padding=True,
+            truncation=True,
+            return_tensors="pt",
+        )
         batch_dict = {k: v.to(self.device) for k, v in batch_dict.items()}
 
         outputs = self.model(**batch_dict)
-        embeddings = self.average_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
+        embeddings = self.average_pool(
+            outputs.last_hidden_state, batch_dict["attention_mask"]
+        )
         return embeddings.detach().cpu().numpy()
 
     def encode(self, input_texts: List[str], batch_size=8):
@@ -58,11 +72,10 @@ class E5Model:
         return np.array(result)
 
 
-
 def initialize_model(model, device):
-    if model.startswith('sentence-transformers/'):
+    if model.startswith("sentence-transformers/"):
         return SentenceTransformer(model, device=device)
-    elif 'e5-' in model:
+    elif "e5-" in model:
         return E5Model(model, device)
 
 
@@ -70,27 +83,36 @@ def load_model_cpu(model):
     print("Loading", model, "on cpu")
     return initialize_model(model, "cpu")
 
+
 def load_model(model):
-    if os.path.exists('/tmp/.use-cpu'):
+    if torch.backends.mps.is_available():
+        device = "mps"
+    elif torch.backends.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+    if os.path.exists("/tmp/.use-cpu"):
         return load_model_cpu(model)
     else:
         try:
-            return initialize_model(model, 'cuda')
+            return initialize_model(model, device)
         except Exception as exc:
             import traceback
 
             traceback.print_tb(exc)
             return load_model_cpu(model)
-    
 
 
 models = {
-    'sentence-transformers/all-mpnet-base-v2': load_model('sentence-transformers/all-mpnet-base-v2'),
-    'intfloat/e5-large-v2': load_model('intfloat/e5-large-v2'),
+    "sentence-transformers/all-mpnet-base-v2": load_model(
+        "sentence-transformers/all-mpnet-base-v2"
+    ),
+    "intfloat/e5-large-v2": load_model("intfloat/e5-large-v2"),
 }
 
-cache_backend: CacheBackend = SQLiteCacheBackend('embeddings_cache')
+cache_backend: CacheBackend = SQLiteCacheBackend("embeddings_cache")
 print("Models and cache ready")
+
 
 def yield_from_file(file):
     yield file.getvalue()
@@ -102,12 +124,16 @@ def add_to_cache(cache_backend, transformer, strings, embeddings):
         virt_file = io.BytesIO()
         np.save(virt_file, embedding, allow_pickle=False)  # type: ignore
         virt_file.seek(0)
-        pairs.append((transformer.encode('utf-8') + string.encode('utf-8'), virt_file.read()))
+        pairs.append(
+            (transformer.encode("utf-8") + string.encode("utf-8"), virt_file.read())
+        )
     cache_backend.write_batch(pairs)
 
 
 @app.post("/{transformer:path}")
-async def root(transformer, encode_request: EncodeRequest, background_tasks: BackgroundTasks):
+async def root(
+    transformer, encode_request: EncodeRequest, background_tasks: BackgroundTasks
+):
     # fixme: support sending as binary data
     model = models[transformer]
     # find cached embeddings
@@ -116,7 +142,9 @@ async def root(transformer, encode_request: EncodeRequest, background_tasks: Bac
     embeddings = []
     empty_indexes = []
     for index, string in enumerate(encode_request.input):
-        cached_data = cache_backend.get(transformer.encode('utf-8') + string.encode('utf-8'))
+        cached_data = cache_backend.get(
+            transformer.encode("utf-8") + string.encode("utf-8")
+        )
         if cached_data is None:
             to_embed.append(string)
             empty_indexes.append(index)
@@ -129,11 +157,13 @@ async def root(transformer, encode_request: EncodeRequest, background_tasks: Bac
             encoded_embeddings = model.encode(tuple(to_embed))
         finally:
             import torch.cuda
+
             torch.cuda.empty_cache()
         for i, (string, embedding) in enumerate(zip(to_embed, encoded_embeddings)):
             embeddings[empty_indexes[i]] = embedding
-        background_tasks.add_task(add_to_cache, cache_backend, transformer, to_embed,
-                                  encoded_embeddings)
+        background_tasks.add_task(
+            add_to_cache, cache_backend, transformer, to_embed, encoded_embeddings
+        )
     virt_file = io.BytesIO()
     np.save(virt_file, embeddings, allow_pickle=False)  # type: ignore
     return StreamingResponse(yield_from_file(virt_file))
